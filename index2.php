2fd0c20c41b4d35b937921531b315d6802bb2709
yuhi_deve <yuhinnn@gmail.com>
2016-05-26 18:20:12 +0900

    色の変更終わり

--- index2.php
+++ index2.php
@@ -7,24 +7,32 @@
 </head>
   <body>
 
-<div class="q1">
-<input class="fist" type="button" name="a" value="aaa" onClick="choice(1)">
-<input class="secound"type="button" name="b" value="bbb" onClick="choice(2)">
-<input class="third" type="button" name="c" value="ccc" onClick="choice(3)">
-</div>
+<h1>Question1</h1>
 
+<form name=frm>
+<div class="q1">
+<input id="first" class="button" type="button" name="a" value="aaa" onClick="choice(1)" style="background:yellow"=>
+<input id="secound" class="button"type="button" name="b" value="bbb" onClick="choice(2)" style="background:yellow"=>
+<input id="third" class="button" type="button" name="c" value="ccc" onClick="choice(3)" style="background:yellow"=>
+</div>
+<form>
+
+
+<h1>Question2</h1>
 
 <div class="q2">
-<input type="button" name="d" value="aaa" onClick="choice2(4)">
-<input type="button" name="e" value="bbb" onClick="choice2(5)">
-<input type="button" name="f" value="ccc" onClick="choice2(6)">
+<input id="fourth" type="button" name="d" value="aaa" onClick="choice2(4)" style="background:yellow"=>
+<input id="fifth"  type="button" name="e" value="bbb" onClick="choice2(5)" style="background:yellow"=>
+<input id="sixth" type="button" name="f" value="ccc" onClick="choice2(6)" style="background:yellow"=>
 </div>
 
 
+<h1>Question3</h1>
+
 <div class="q3">
-<input type="button" name="g" value="aaa" onClick="choice3(7)">
-<input type="button" name="i" value="bbb" onClick="choice3(8)">
-<input type="button" name="k" value="ccc" onClick="choice3(9)">
+<input id="seventh" type="button" name="g" value="aaa" onClick="choice3(7)" style="background:yellow"=>
+<input id="eighth" type="button" name="i" value="bbb" onClick="choice3(8)" style="background:yellow"=>
+<input id="ninth" type="button" name="k" value="ccc" onClick="choice3(9)" style="background:yellow"=>
 </div>
 
 <div class="mid">
--- script2.js
+++ script2.js
@@ -3,22 +3,29 @@ var Q2;
 var Q3;
 var sum;
 
-
-
 function choice(btn){
 	if(btn == 1){
 		 Q1 = 1;
 		console.log(Q1);
-	       var box = document.getElementByld('first');
-	       box.style.backgroundColor = "#BBCCDD";
-	}
+	 document.getElementById("first").style.background = "red";
+	 document.getElementById("secound").style.background = "yellow";
+	 document.getElementById("third").style.background = "yellow";
+	}  
 	else if(btn == 2){
 		 Q1 = 2;
-		console.log(Q1);
+		 console.log(Q1);
+	 document.getElementById("first").style.background = "yellow";
+	 document.getElementById("secound").style.background = "red";       
+	 document.getElementById("third").style.background = "yellow";
+
 	}
 	else if(btn == 3){
 		Q1 = 3;
 		console.log(Q1);
+	 document.getElementById("first").style.background = "yellow";
+	 document.getElementById("secound").style.background = "yellow";       
+	 document.getElementById("third").style.background= "red"; 
+	
 	}
 }
 
@@ -26,30 +33,50 @@ function choice2(btn){
 	if(btn == 4){
 		Q2 = 1;
 		console.log(Q2);
+	 document.getElementById("fourth").style.background = "red";
+	 document.getElementById("fifth").style.background = "yellow";       
+	 document.getElementById("sixth").style.background= "yellow"; 
 	}
 	else if(btn == 5){
 		Q2 = 2;
 		console.log(Q2);
+	 document.getElementById("fourth").style.background = "yellow";
+	 document.getElementById("fifth").style.background = "red";       
+	 document.getElementById("sixth").style.background= "yellow"; 
 	}
 	else if(btn == 6){
 		Q2 = 3;
 		console.log(Q2);
+	 document.getElementById("fourth").style.background = "yellow";
+	 document.getElementById("fifth").style.background = "yellow";       
+	 document.getElementById("sixth").style.background= "red"; 
 	}
+
 }
 
 function choice3(btn){
 	if(btn == 7){
 		Q3 = 1;
 		console.log(Q3);
+	 document.getElementById("seventh").style.background = "red";
+	 document.getElementById("eighth").style.background = "yellow";       
+	 document.getElementById("ninth").style.background = "yellow";
 	}
 	else if(btn == 8){
 		Q3 = 2;
 		console.log(Q3);
+	 document.getElementById("seventh").style.background = "yellow";
+	 document.getElementById("eighth").style.background = "red";       
+	 document.getElementById("ninth").style.background = "yellow";
 	}
 	else if(btn == 9){
 		Q3 = 3;
 		console.log(Q3);
+	 document.getElementById("seventh").style.background = "yellow";
+	 document.getElementById("eighth").style.background = "yellow";       
+	 document.getElementById("ninth").style.background = "red";
 	}
+
 }
 
 function mid(){
@@ -59,6 +86,6 @@ function mid(){
 
 
 function result(){
-      var sum=Q1+Q2+Q3;
+      sum=Q1+Q2+Q3;
 	console.log(sum);
 }
